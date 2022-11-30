b5a3cd74e922c2afe78c54e7a0e4b00767f8a4a4
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-11-30 09:41:18 +0530

    Create pattern3.cpp

---
+++ starpattern/pattern3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {
+   int n;
+   cin>>n;
+   for(int=1;i<=n;i++)
+   {   
+     for(int j=1;j<=n;j++)
+     {
+       cout<<i<<" ";
+     }
+     cout<<endl;
+   }
+   return 0;
+ }
+/* tc-O(N)
+SC-O(1)
+*/
+/*  O/P
+    1 1 1 1 1
+    2 2 2 2 2
+    3 3 3 3 3
+    4 4 4 4 5
+    */
