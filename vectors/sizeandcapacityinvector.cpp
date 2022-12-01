27dde56bb1e4a2c7b3838b85d4ef3521123de706
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-01 18:13:51 +0530

    Create sizeandcapacityinvector.cpp

---
+++ vectors/sizeandcapacityinvector.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  vectors<int>v;
+  cout<<v.size()<<endl; // to check no of elements in vector
+  cout<<v.capacity(); // to check how much elements vector can contain
+  return 0;
+}
+/*
+O/P
+0
+0
