0f22096afdc19491fc974cd64766f5f2717e94c3
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-01 18:26:51 +0530

    Update toinsertelementinvector.cpp

--- vectors/toinsertelementinvector.cpp
+++ vectors/toinsertelementinvector.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
   vector<int>v;
-  v.push_back(3);// to insert element in the end of vector we use this function no of element insert are 3 at end .
+  v.push_back(1);// to insert element in the end of vector we use this function no of element insert are 3 at end .
   cout<<v.size()<<endl;
   cout<<v.capacity();
   return 0;
 }
 /* O/P
-3
-3
+1
+1
 */
