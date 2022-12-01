fd5d372562f6acdc405d0d205e6ca1cfd821b456
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-01 18:09:53 +0530

    Create vectorsinc++.cpp

---
+++ vectorsinc++.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  vectors<int>v;
+  v.push_back(2);
+  cout<<v.size()<<endl;
+  cout<<v.capacity();
+  return 0;
+}
+
+
+/*
+o/p
+2
+2
+*/
