d71f71322f6e1682757fb8d3a660a8671f08c6b2
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-07 16:00:17 +0530

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -1,7 +1,6 @@
-#include <bits/stc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
+
   struct mahima
   {
     int a;
@@ -9,11 +8,13 @@ int main()
     float c;
     double d;
     string s;
-    
-    struct mahima s1;
+  };
+    int main()
+    {
+    struct mahima s1={10,'a',2.23f,1.2323434,"mahimabhardwaj"};
     cout<<s1.a<<endl;
     cout<<s1.b<<endl;
     cout<<s1.c<<endl;
-    cout<<s1.d<<endl;
+    cout<<setprecision(8)<<s1.d<<endl;
     cout<<s1.s<<endl;
   }
