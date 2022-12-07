3a328cbe32633627062f0ced890e42b83ebaa13c
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-07 15:54:26 +0530

    Create struct.cpp

---
+++ struct.cpp
@@ -0,0 +1,19 @@
+#include <bits/stc++.h>
+using namespace std;
+int main()
+{
+  struct mahima
+  {
+    int a;
+    char b;
+    float c;
+    double d;
+    string s;
+    
+    struct mahima s1;
+    cout<<s1.a<<endl;
+    cout<<s1.b<<endl;
+    cout<<s1.c<<endl;
+    cout<<s1.d<<endl;
+    cout<<s1.s<<endl;
+  }
